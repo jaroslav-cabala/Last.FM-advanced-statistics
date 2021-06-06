d6fba4775f09eb6eb8b9389ae433b38bac3807ae
jaroslav-cabala <63345554+jaroslav-cabala@users.noreply.github.com>
2021-06-07 01:42:56 +0200

    code made prettier 2

--- app/src/files/testScrobbles.ts
+++ app/src/files/testScrobbles.ts
@@ -1,7 +1,6 @@
 import { Album, Artist, Scrobbles } from "../models/domain";
 
-export const testGetRecentTracksResponseJSONPage1 = 
-`{
+export const testGetRecentTracksResponseJSONPage1 = `{
   "recenttracks": {
       "@attr": {
           "page": "1",
@@ -230,8 +229,7 @@ export const testGetRecentTracksResponseJSONPage1 =
   }
 }`;
 
-export const testGetRecentTracksResponseJSONPage2 =
-`{
+export const testGetRecentTracksResponseJSONPage2 = `{
   "recenttracks": {
       "@attr": {
           "page": "2",
@@ -425,282 +423,280 @@ export const testGetRecentTracksResponseJSONPage2 =
   }
 }`;
 
-export const testDeserializedScrobblesPage1: Scrobbles =
-[
+export const testDeserializedScrobblesPage1: Scrobbles = [
   {
-    "mbid": "6e1adb03-7d8f-4508-9c60-fb0ab6513057",
-    "name": "C'est Mon Dernier Pigeon",
-    "album": {
-      "mbid": "70e5e674-7412-49b3-9532-c389fa64e138",
-      "name": "The Blind Leading The Blind",
-      "artist": {
-        "mbid": "eda0c0a6-c3ce-426d-8f84-e3200bc02693",
-        "name": "1914",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "6e1adb03-7d8f-4508-9c60-fb0ab6513057",
+    name: "C'est Mon Dernier Pigeon",
+    album: {
+      mbid: "70e5e674-7412-49b3-9532-c389fa64e138",
+      name: "The Blind Leading The Blind",
+      artist: {
+        mbid: "eda0c0a6-c3ce-426d-8f84-e3200bc02693",
+        name: "1914",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "eda0c0a6-c3ce-426d-8f84-e3200bc02693",
-      "name": "1914",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "eda0c0a6-c3ce-426d-8f84-e3200bc02693",
+      name: "1914",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/1914/_/C%27est+Mon+Dernier+Pigeon",
-    "date": "1623015324"
+    url: "https://www.last.fm/music/1914/_/C%27est+Mon+Dernier+Pigeon",
+    date: "1623015324",
   },
   {
-    "mbid": "05199f35-5a7f-407b-8657-3d0422149e2b",
-    "name": "Sisyphus Bliss",
-    "album": {
-      "mbid": "1b0cc3f4-2d99-4ec7-af59-dfe2d6f73b2a",
-      "name": "Memento Mori",
-      "artist": {
-        "mbid": "114a7dd2-00bb-4666-a650-367a2fb08b35",
-        "name": "Aephanemer",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "05199f35-5a7f-407b-8657-3d0422149e2b",
+    name: "Sisyphus Bliss",
+    album: {
+      mbid: "1b0cc3f4-2d99-4ec7-af59-dfe2d6f73b2a",
+      name: "Memento Mori",
+      artist: {
+        mbid: "114a7dd2-00bb-4666-a650-367a2fb08b35",
+        name: "Aephanemer",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "114a7dd2-00bb-4666-a650-367a2fb08b35",
-      "name": "Aephanemer",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "114a7dd2-00bb-4666-a650-367a2fb08b35",
+      name: "Aephanemer",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Aephanemer/_/Sisyphus+Bliss",
-    "date": "1623015021"
+    url: "https://www.last.fm/music/Aephanemer/_/Sisyphus+Bliss",
+    date: "1623015021",
   },
   {
-    "mbid": "cc595bb1-2c4d-410f-87cc-9d4f6f667f70",
-    "name": "Paradogma",
-    "album": {
-      "mbid": "1bc30f2a-2c69-45b9-9ef6-4b03f4b09fb1",
-      "name": "Paradogma",
-      "artist": {
-        "mbid": "b629afc0-2d61-4638-9e49-32bcacf69228",
-        "name": "Hour of Penance",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "cc595bb1-2c4d-410f-87cc-9d4f6f667f70",
+    name: "Paradogma",
+    album: {
+      mbid: "1bc30f2a-2c69-45b9-9ef6-4b03f4b09fb1",
+      name: "Paradogma",
+      artist: {
+        mbid: "b629afc0-2d61-4638-9e49-32bcacf69228",
+        name: "Hour of Penance",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "b629afc0-2d61-4638-9e49-32bcacf69228",
-      "name": "Hour of Penance",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "b629afc0-2d61-4638-9e49-32bcacf69228",
+      name: "Hour of Penance",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Hour+of+Penance/_/Paradogma",
-    "date": "1623014761"
+    url: "https://www.last.fm/music/Hour+of+Penance/_/Paradogma",
+    date: "1623014761",
   },
   {
-    "mbid": "713b24ef-0621-3764-8e0a-ea689444aaa8",
-    "name": "Tyrant",
-    "album": {
-      "mbid": "c20668c5-0a48-4d4b-907a-ad76e5f1cb9d",
-      "name": "Abandon All Life",
-      "artist": {
-        "mbid": "a7d9c5f6-751d-4ea5-bf6d-39f1751d44a1",
-        "name": "Nails",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "713b24ef-0621-3764-8e0a-ea689444aaa8",
+    name: "Tyrant",
+    album: {
+      mbid: "c20668c5-0a48-4d4b-907a-ad76e5f1cb9d",
+      name: "Abandon All Life",
+      artist: {
+        mbid: "a7d9c5f6-751d-4ea5-bf6d-39f1751d44a1",
+        name: "Nails",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "a7d9c5f6-751d-4ea5-bf6d-39f1751d44a1",
-      "name": "Nails",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "a7d9c5f6-751d-4ea5-bf6d-39f1751d44a1",
+      name: "Nails",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Nails/_/Tyrant",
-    "date": "1623014511"
+    url: "https://www.last.fm/music/Nails/_/Tyrant",
+    date: "1623014511",
   },
   {
-    "mbid": "b3148d9f-76b0-3fb6-beac-7823b4b18b06",
-    "name": "Sophistic Demise",
-    "album": {
-      "mbid": "088beb9f-bf90-4712-b7b9-3eec3f285cab",
-      "name": "Oracles",
-      "artist": {
-        "mbid": "48cb4248-889d-45ed-8462-acb29c002a8d",
-        "name": "Fleshgod Apocalypse",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "b3148d9f-76b0-3fb6-beac-7823b4b18b06",
+    name: "Sophistic Demise",
+    album: {
+      mbid: "088beb9f-bf90-4712-b7b9-3eec3f285cab",
+      name: "Oracles",
+      artist: {
+        mbid: "48cb4248-889d-45ed-8462-acb29c002a8d",
+        name: "Fleshgod Apocalypse",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "48cb4248-889d-45ed-8462-acb29c002a8d",
-      "name": "Fleshgod Apocalypse",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "48cb4248-889d-45ed-8462-acb29c002a8d",
+      name: "Fleshgod Apocalypse",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Fleshgod+Apocalypse/_/Sophistic+Demise",
-    "date": "1621378724"
-  }
+    url: "https://www.last.fm/music/Fleshgod+Apocalypse/_/Sophistic+Demise",
+    date: "1621378724",
+  },
 ];
 
-export const testDeserializedScrobblesPage2: Scrobbles =
-[
+export const testDeserializedScrobblesPage2: Scrobbles = [
   {
-    "mbid": "7b986361-7ca9-45a7-b0f9-ad972bd1df70",
-    "name": "Zamawianie drugie",
-    "album": {
-      "mbid": "22702eb6-4c35-45d4-932c-f44309ef98a5",
-      "name": "Nocel",
-      "artist": {
-        "mbid": "48638996-5311-40af-87a7-a1a6c0072944",
-        "name": "Furia",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "7b986361-7ca9-45a7-b0f9-ad972bd1df70",
+    name: "Zamawianie drugie",
+    album: {
+      mbid: "22702eb6-4c35-45d4-932c-f44309ef98a5",
+      name: "Nocel",
+      artist: {
+        mbid: "48638996-5311-40af-87a7-a1a6c0072944",
+        name: "Furia",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "48638996-5311-40af-87a7-a1a6c0072944",
-      "name": "Furia",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "48638996-5311-40af-87a7-a1a6c0072944",
+      name: "Furia",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Furia/_/Zamawianie+drugie",
-    "date": "1623014404"
+    url: "https://www.last.fm/music/Furia/_/Zamawianie+drugie",
+    date: "1623014404",
   },
   {
-    "mbid": "07e86f84-1977-4340-9747-8e8c101c3d63",
-    "name": "The Cosmic Vein",
-    "album": {
-      "mbid": "982e0117-6d82-443d-b184-e56cb259e99c",
-      "name": "Trumpeting Ecstasy",
-      "artist": {
-        "mbid": "af2373bf-4b18-4e06-954c-ab83946992c9",
-        "name": "Full of Hell",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "07e86f84-1977-4340-9747-8e8c101c3d63",
+    name: "The Cosmic Vein",
+    album: {
+      mbid: "982e0117-6d82-443d-b184-e56cb259e99c",
+      name: "Trumpeting Ecstasy",
+      artist: {
+        mbid: "af2373bf-4b18-4e06-954c-ab83946992c9",
+        name: "Full of Hell",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "af2373bf-4b18-4e06-954c-ab83946992c9",
-      "name": "Full of Hell",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "af2373bf-4b18-4e06-954c-ab83946992c9",
+      name: "Full of Hell",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Full+of+Hell/_/The+Cosmic+Vein",
-    "date": "1623014174"
+    url: "https://www.last.fm/music/Full+of+Hell/_/The+Cosmic+Vein",
+    date: "1623014174",
   },
   {
-    "mbid": "3b114bab-d37b-305c-9419-e3cb7be196f7",
-    "name": "For the Revolution",
-    "album": {
-      "mbid": "5c69d3e6-76b0-42ab-b438-5e6d9a509f8e",
-      "name": "For the Revolution",
-      "artist": {
-        "mbid": "8d5291bc-e960-428a-9004-b5ab9c171dd2",
-        "name": "Kalmah",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "3b114bab-d37b-305c-9419-e3cb7be196f7",
+    name: "For the Revolution",
+    album: {
+      mbid: "5c69d3e6-76b0-42ab-b438-5e6d9a509f8e",
+      name: "For the Revolution",
+      artist: {
+        mbid: "8d5291bc-e960-428a-9004-b5ab9c171dd2",
+        name: "Kalmah",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "8d5291bc-e960-428a-9004-b5ab9c171dd2",
-      "name": "Kalmah",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "8d5291bc-e960-428a-9004-b5ab9c171dd2",
+      name: "Kalmah",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Kalmah/_/For+the+Revolution",
-    "date": "1623014041"
+    url: "https://www.last.fm/music/Kalmah/_/For+the+Revolution",
+    date: "1623014041",
   },
   {
-    "mbid": "4eff0f9b-348d-4365-9280-ad34d67c3f52",
-    "name": "Åpenbaring",
-    "album": {
-      "mbid": "48ff1b5f-cbc2-4301-a40f-325cb4f89760",
-      "name": "Meir",
-      "artist": {
-        "mbid": "61fdc79c-df1a-4999-8d2f-668f18c11283",
-        "name": "Kvelertak",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "4eff0f9b-348d-4365-9280-ad34d67c3f52",
+    name: "Åpenbaring",
+    album: {
+      mbid: "48ff1b5f-cbc2-4301-a40f-325cb4f89760",
+      name: "Meir",
+      artist: {
+        mbid: "61fdc79c-df1a-4999-8d2f-668f18c11283",
+        name: "Kvelertak",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "61fdc79c-df1a-4999-8d2f-668f18c11283",
-      "name": "Kvelertak",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "61fdc79c-df1a-4999-8d2f-668f18c11283",
+      name: "Kvelertak",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Kvelertak/_/%C3%85penbaring",
-    "date": "1623013685"
+    url: "https://www.last.fm/music/Kvelertak/_/%C3%85penbaring",
+    date: "1623013685",
   },
   {
-    "mbid": "0e703983-2bbc-4c7f-9821-7877a63e759e",
-    "name": "Apokatastasis Pantôn",
-    "album": {
-      "mbid": "0af6c6b3-92e3-45d7-8b37-c589bd61281c",
-      "name": "Paracletus",
-      "artist": {
-        "mbid": "37ea73dd-a48c-4659-8b9d-5c77bd9cbd12",
-        "name": "Deathspell Omega",
-        "country": "",
-        "image": "",
-        "url": ""
+    mbid: "0e703983-2bbc-4c7f-9821-7877a63e759e",
+    name: "Apokatastasis Pantôn",
+    album: {
+      mbid: "0af6c6b3-92e3-45d7-8b37-c589bd61281c",
+      name: "Paracletus",
+      artist: {
+        mbid: "37ea73dd-a48c-4659-8b9d-5c77bd9cbd12",
+        name: "Deathspell Omega",
+        country: "",
+        image: "",
+        url: "",
       },
-      "releaseDate": "",
-      "image": "",
-      "url": ""
+      releaseDate: "",
+      image: "",
+      url: "",
     },
-    "artist": {
-      "mbid": "37ea73dd-a48c-4659-8b9d-5c77bd9cbd12",
-      "name": "Deathspell Omega",
-      "country": "",
-      "image": "",
-      "url": ""
+    artist: {
+      mbid: "37ea73dd-a48c-4659-8b9d-5c77bd9cbd12",
+      name: "Deathspell Omega",
+      country: "",
+      image: "",
+      url: "",
     },
-    "url": "https://www.last.fm/music/Deathspell+Omega/_/Apokatastasis+Pant%C3%B4n",
-    "date": "1623013507"
-  }
+    url: "https://www.last.fm/music/Deathspell+Omega/_/Apokatastasis+Pant%C3%B4n",
+    date: "1623013507",
+  },
 ];
 
 export const testScrobbles: Scrobbles = [
@@ -806,5 +802,5 @@ export const testScrobbles2: Scrobbles = [
     } as Artist,
     url: "bgfvferegrhytjyhyt145gt",
     date: "1621423019",
-  }
+  },
 ];
